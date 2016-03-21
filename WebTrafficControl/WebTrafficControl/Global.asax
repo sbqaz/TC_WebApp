4a0fdfb99cdd9a2b384b00edca654b08952be178
Cecilie Moriat <ceciliemoriat@gmail.com>
2016-03-21 12:29:15 +0100

    Design implemented, login site under construction :tada:

---
+++ WebTrafficControl/WebTrafficControl/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebTrafficControl.MvcApplication" Language="C#" %>
---
+++ WebTrafficControl/WebTrafficControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTrafficControl.Startup))]
+namespace WebTrafficControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
