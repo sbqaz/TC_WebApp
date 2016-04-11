786d161a26b4d61cd2aacadfc5224b04f1b50942
Cecilie Moriat <ceciliemoriat@gmail.com>
2016-04-11 09:11:40 +0200

    Design (5500 files)

--- TC-01/TC-01/Controllers/AccountController.cs
+++ TC-01/TC-01/Controllers/AccountController.cs
@@ -75,9 +75,9 @@ namespace TC_01.Controllers
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+			// This doesn't count login failures towards account lockout
+			// To enable password failures to trigger account lockout, change to shouldLockout: true
+			var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -395,7 +395,7 @@ namespace TC_01.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Account", "Login");
         }
 
         //
