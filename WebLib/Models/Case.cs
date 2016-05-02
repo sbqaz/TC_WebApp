ae3e4e1d70a94dfca41c63a641e8ef1433cae466
bork91 <Morten@Mortens-MacBook-Pro.local>
2016-05-02 10:21:01 +0200

    Enum added

--- WebLib/Models/Case.cs
+++ WebLib/Models/Case.cs
@@ -13,7 +13,22 @@ namespace WebLib.Models
             done = 2,
             pending = 3
         }
-        
+
+        public enum ObserverSelection
+        {
+            police = 0,
+            user = 1,
+            thirdPart = 2,
+            own = 3,
+            alarm = 4,
+            lightError = 5,
+            toneSignal = 6,
+            pedestrianPush = 7,
+            shutdownError = 8,
+            trafficInjury = 9,
+            detectorError = 10
+        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
@@ -23,6 +38,22 @@ namespace WebLib.Models
         public int Status { get; set; }
         public string Worker { get; set; }
         public DateTime Time { get; set; }
+
+        /// <summary>
+        /// Observer that reportet the case.
+        /// police = 0
+        /// user = 1
+        /// thirdPart = 2
+        /// own = 3
+        /// alarm = 4
+        /// lightError = 5
+        /// toneSignal = 6
+        /// pedestrianPush = 7
+        /// shutdownError = 8
+        /// trafficInjury = 9
+        /// detectorError = 10
+        /// </summary>
+        [Range(0, 10)]
         public int Observer { get; set; }
         public string ErrorDescription { get; set; }
         public string MadePepair { get; set; }
--- WebLib/Models/Installation.cs
+++ WebLib/Models/Installation.cs
@@ -6,11 +6,25 @@ namespace WebLib.Models
 {
     public class Installation
     {
+        public enum InstalStatus
+        {
+            Green = 0,
+            Yellow = 1,
+            Red = 2
+        }
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        /// <summary>
+        /// Status of the installation.
+        /// Green = 0
+        /// Yellow = 1
+        /// Red = 2
+        /// </summary>
+        public int Status { get; set; }
         public Position Position { get; set; }
     }
 }
