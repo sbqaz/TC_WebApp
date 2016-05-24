fcd6862618ce0fe004765069e37ee7875e051d74
SB <201370518@post.au.dk>
2016-05-24 16:13:35 +0200

    Forced https

--- WebSite/App_Start/FilterConfig.cs
+++ WebSite/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace WebSite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
